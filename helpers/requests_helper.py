d149b98c8354da99f46dfd9288bf23ce84cb8045
RuslanGitelman <47743960+RuslanGitelman@users.noreply.github.com>
2021-07-02 14:06:40 +0300

    integration commit

--- helpers/requests_helper.py
+++ helpers/requests_helper.py
@@ -18,3 +18,23 @@ def send_get_request(session, url, request_name, filename=None):
         assert_status_code(response)
         if response.content:
             return response.json()
+
+
+def send_post_request(session, url, request_name, filename, status_code=200):
+    source_file = Path("data") / filename
+    with open(source_file, "rb") as data:
+        with session.post(url, name=request_name, data=data, verify=False,
+                          catch_response=True) as response:
+            assert_status_code(response, status_code)
+            if response.content:
+                return response.json()
+
+
+def send_patch_request(session, url, request_name, filename, status_code=200):
+    source_file = Path("data") / filename
+    with open(source_file, "rb") as data:
+        with session.patch(url, name=request_name, data=data, verify=False,
+                           catch_response=True) as response:
+            assert_status_code(response, status_code)
+            if response.content:
+                return response.json()
--- load_all_in_one_admin_locustfile.py
+++ load_all_in_one_admin_locustfile.py
@@ -6,7 +6,7 @@ import urllib3
 
 from helpers.json_helper import read_json
 from helpers.auth_helper import AuthorizationHelper
-from helpers.requests_helper import send_get_request
+from helpers.requests_helper import send_get_request, send_patch_request, send_post_request
 
 urllib3.disable_warnings()
 
@@ -142,6 +142,190 @@ class AdminBehavior(TaskSet):
         endpoint = target["admin"]["get_system_maturity_export"]["endpoint"]
         name = "/SYSTEM MATURITY EXPORT"
         send_get_request(self.client, endpoint, name)
+
+    # @task
+    def get_system_maturity(self):
+        endpoint = target["admin"]["get_system_maturity"]["endpoint"]
+        name = "/SYSTEM MATURITY"
+        filename = target["admin"]["get_system_maturity"]["filename"]
+        send_get_request(self.client, endpoint, name, filename)
+
+    # @task   ## 504
+    def get_system_overview_export(self):
+        endpoint = target["admin"]["get_system_overview_export"]["endpoint"]
+        name = "/SYSTEM OVERVIEW EXPORT"
+        send_get_request(self.client, endpoint, name)
+
+    # @task   ## 504
+    def get_system_overview(self):
+        endpoint = target["admin"]["get_system_overview"]["endpoint"]
+        name = "/SYSTEM OVERVIEW"
+        filename = target["admin"]["get_system_overview"]["filename"]
+        send_get_request(self.client, endpoint, name, filename)
+
+    # @task   ## 500
+    def patch_request_by_id(self):
+        endpoint = target["admin"]["patch_request_by_id"]["endpoint"]
+        request_id = target["admin"]["patch_request_by_id"]["request_id"]
+        endpoint += request_id
+        name = "/REQUEST BY ID"
+        filename = target["admin"]["patch_request_by_id"]["filename"]
+        send_patch_request(self.client, endpoint, name, filename)
+
+    # @task
+    def post_owt_request(self):
+        endpoint = target["admin"]["post_owt_request"]["endpoint"]
+        user_id = target["authorization"]["user"]["user_id"]
+        endpoint += user_id
+        name = "/OWT REQUEST"
+        filename = target["admin"]["post_owt_request"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task
+    def get_user_sort(self):
+        endpoint = target["admin"]["get_user_sort"]["endpoint"]
+        name = "/USER SORT"
+        filename = target["admin"]["get_user_sort"]["filename"]
+        send_get_request(self.client, endpoint, name, filename)
+
+    # @task
+    def get_request_by_id(self):
+        endpoint = target["admin"]["get_request_by_id"]["endpoint"]
+        request_id = target["admin"]["get_request_by_id"]["request_id"]
+        endpoint += request_id
+        name = "/REQUEST BY ID"
+        rs = send_get_request(self.client, endpoint, name)
+        pprint(rs)
+
+    # @task
+    def post_owt_request_preview(self):
+        endpoint = target["admin"]["post_owt_request_preview"]["endpoint"]
+        user_id = target["authorization"]["user"]["user_id"]
+        endpoint += user_id
+        name = "/OWT REQUEST PREVIEW"
+        filename = target["admin"]["post_owt_request_preview"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task
+    def post_tba_request(self):
+        endpoint = target["admin"]["post_tba_request"]["endpoint"]
+        user_id = target["authorization"]["user"]["user_id"]
+        endpoint += user_id
+        name = "/TBA REQUEST"
+        filename = target["admin"]["post_tba_request"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task
+    def post_tba_request_preview(self):
+        endpoint = target["admin"]["post_tba_request_preview"]["endpoint"]
+        user_id = target["authorization"]["user"]["user_id"]
+        endpoint += user_id
+        name = "/TBA REQUEST PREVIEW"
+        filename = target["admin"]["post_tba_request_preview"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task
+    def post_tbu_request(self):
+        endpoint = target["admin"]["post_tbu_request"]["endpoint"]
+        user_id = target["authorization"]["user"]["user_id"]
+        endpoint += user_id
+        name = "/TBU REQUEST"
+        filename = target["admin"]["post_tbu_request"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task
+    def post_tbu_request_preview(self):
+        endpoint = target["admin"]["post_tbu_request_preview"]["endpoint"]
+        user_id = target["authorization"]["user"]["user_id"]
+        endpoint += user_id
+        name = "/TBU REQUEST PREVIEW"
+        filename = target["admin"]["post_tbu_request_preview"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task
+    def post_ca_request(self):
+        endpoint = target["admin"]["post_ca_request"]["endpoint"]
+        name = "/CA REQUEST"
+        filename = target["admin"]["post_ca_request"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task
+    def post_ca_request_preview(self):
+        endpoint = target["admin"]["post_ca_request_preview"]["endpoint"]
+        name = "/CA REQUEST PREVIEW"
+        filename = target["admin"]["post_ca_request_preview"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task
+    def post_da_request(self):
+        endpoint = target["admin"]["post_da_request"]["endpoint"]
+        name = "/DA REQUEST"
+        filename = target["admin"]["post_da_request"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task
+    def post_da_request_preview(self):
+        endpoint = target["admin"]["post_da_request_preview"]["endpoint"]
+        name = "/DA REQUEST PREVIEW"
+        filename = target["admin"]["post_da_request_preview"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task
+    def post_dra_request(self):
+        endpoint = target["admin"]["post_dra_request"]["endpoint"]
+        name = "/DRA REQUEST"
+        filename = target["admin"]["post_dra_request"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task
+    def post_dra_request_preview(self):
+        endpoint = target["admin"]["post_dra_request_preview"]["endpoint"]
+        name = "/DRA REQUEST PREVIEW"
+        filename = target["admin"]["post_dra_request_preview"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task   # 502
+    def post_messages_send_to_all(self):
+        endpoint = target["admin"]["post_messages_send_to_all"]["endpoint"]
+        name = "/MESSAGES SENT TO ALL"
+        filename = target["admin"]["post_messages_send_to_all"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task   # 502
+    def post_messages_send_to_group(self):
+        endpoint = target["admin"]["post_messages_send_to_group"]["endpoint"]
+        name = "/MESSAGES SENT TO GROUP"
+        filename = target["admin"]["post_messages_send_to_group"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task  # 502
+    def post_messages_send_to_users(self):
+        endpoint = target["admin"]["post_messages_send_to_users"]["endpoint"]
+        name = "/MESSAGES SENT TO USERS"
+        filename = target["admin"]["post_messages_send_to_users"]["filename"]
+        send_post_request(self.client, endpoint, name, filename)
+
+    # @task   # 0
+    def get_transfer_requests_export(self):
+        endpoint = target["admin"]["get_transfer_requests_export"]["endpoint"]
+        name = "/TRANSFER REQUESTS EXPORT"
+        filename = target["admin"]["get_transfer_requests_export"]["filename"]
+        send_get_request(self.client, endpoint, name, filename)
+
+    # @task   # 400
+    def get_transactions_export(self):
+        endpoint = target["admin"]["get_transactions_export"]["endpoint"]
+        name = "/TRANSACTIONS EXPORT"
+        filename = target["admin"]["get_transactions_export"]["filename"]
+        send_get_request(self.client, endpoint, name, filename)
+
+    # @task   # 500
+    def get_reports_transaction(self):
+        endpoint = target["admin"]["get_reports_transaction"]["endpoint"]
+        name = "/TRANSACTIONS REPORT"
+        filename = target["admin"]["get_reports_transaction"]["filename"]
+        rs = send_get_request(self.client, endpoint, name, filename)
+        pprint(rs)
 
 
 class LoadTestUser(HttpUser):
